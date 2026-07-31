9afbe05e5ae5f8e2cb79ded437362022b07fd43e
moliveiramyers <moliveiramyers@byupathway.edu>
2026-07-31 15:35:49 +0100

    fixed db connection

--- src/config/db.js
+++ src/config/db.js
@@ -4,24 +4,20 @@ import dns from 'dns';
 dns.setServers(['8.8.8.8']);
 
 const connectDB = async () => {
-
-     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
-           dbName: 'campus-connect'
-        });
-        console.log(`MongoDB Connected: ${connection.connection.host}`);
-        } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-     }
-
-
-    if (!mongoUri) {
+    if (!process.env.MONGO_URI) {
         throw new Error('MONGODB_URI is required to connect to MongoDB.');
     }
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: 'campus-connect'
+        });
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
 
-    const connection = await mongoose.connect(mongoUri);
-    console.log(`MongoDB Connected: ${connection.connection.host}`);
-};
 
+
+}
 export default connectDB;
